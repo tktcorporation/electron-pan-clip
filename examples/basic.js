166b399c7b509b6c2c16baff49f59c09ce7774d0
tktcorporation <tktcorporation.go@gmail.com>
2025-04-20 16:45:35 +0000

    chore: Biome設定ファイルの更新、TypeScriptファイルのインポート整理、Node.jsモジュールのインポート方法の変更、スクリプトの簡略化

--- examples/basic.js
+++ examples/basic.js
@@ -1,15 +1,12 @@
-const { copyFiles } = require('electron-pan-clip');
+const { copyFiles } = require("electron-pan-clip");
 
 // 複数のファイルをクリップボードにコピーする例
 try {
-  // ファイルパスはOSに合わせて適切に指定してください
-  const filePaths = [
-    '/path/to/file1.txt',
-    '/path/to/file2.jpg'
-  ];
-  
-  copyFiles(filePaths);
-  console.log('ファイルをクリップボードにコピーしました');
+	// ファイルパスはOSに合わせて適切に指定してください
+	const filePaths = ["/path/to/file1.txt", "/path/to/file2.jpg"];
+
+	copyFiles(filePaths);
+	console.log("ファイルをクリップボードにコピーしました");
 } catch (error) {
-  console.error('エラーが発生しました:', error);
-} 
+	console.error("エラーが発生しました:", error);
+}
